f0270e112810a049f356d3ba4064333dd131bb88
0xngmi <0xngmi@protonmail.com>
2022-02-06 18:31:58 +0000

    build merge function

--- contracts/Tubbies.sol
+++ contracts/Tubbies.sol
@@ -149,29 +149,99 @@ contract Tubbies is ERC721, MultisigOwnable, VRFConsumerBase {
         }
     }
 
-    function getShuffledTokenId(uint id, uint batch) view private returns (uint) {
-        uint finalId = id;
-        uint randomSeed = batchToSeed[batch];
-        finalId = (finalId + randomSeed) % TOKEN_LIMIT;
-        bool[TOKEN_LIMIT/REVEAL_BATCH_SIZE] memory checkedBatches;
-        for(uint i=0; i<batch; i++){
-            for(uint j=0; j<batch; j++){
-                if(checkedBatches[j] == false){
-                    uint batchSeed = batchToSeed[j];
-                    if(batchSeed < finalId && finalId < (batchSeed + REVEAL_BATCH_SIZE)){
-                        finalId = (finalId + REVEAL_BATCH_SIZE) % TOKEN_LIMIT;
-                        checkedBatches[j] = true;
-                    }
-                    if(finalId < REVEAL_BATCH_SIZE && batchSeed > (TOKEN_LIMIT - REVEAL_BATCH_SIZE)){
-                        uint end = (batchSeed + REVEAL_BATCH_SIZE) % TOKEN_LIMIT;
-                        if(finalId < end){
-                            finalId = (finalId + REVEAL_BATCH_SIZE) % TOKEN_LIMIT;
-                            checkedBatches[j] = true;
-                        }
-                    }
-                }
+    struct Range{
+        int128 start;
+        int128 end;
+    }
+
+    // Forked from openzeppelin
+    /**
+     * @dev Returns the largest of two numbers.
+     */
+    function max(int128 a, int128 b) internal pure returns (int128) {
+        return a >= b ? a : b;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(int128 a, int128 b) internal pure returns (int128) {
+        return a < b ? a : b;
+    }
+
+    uint constant RANGE_LENGTH = (TOKEN_LIMIT/REVEAL_BATCH_SIZE)*2;
+    int128 constant intTOKEN_LIMIT = int128(int(TOKEN_LIMIT));
+
+    // ranges include the start but not the end [start, end)
+    function addRange(Range[RANGE_LENGTH] memory ranges, int128 start, int128 end, uint lastIndex) pure private returns (uint) {
+        uint positionToAssume = lastIndex;
+        for(uint j=0; j<lastIndex; j++){
+            int128 rangeStart = ranges[j].start;
+            int128 rangeEnd = ranges[j].end;
+            if(start < rangeStart && positionToAssume == lastIndex){
+                positionToAssume = j;
+            }
+            if(
+                (start < rangeStart && end > rangeStart) ||
+                (rangeStart <= start &&  end <= rangeEnd) ||
+                (start < rangeEnd && end > rangeEnd)
+            ){
+                int128 length = end-start;
+                start = min(start, rangeStart);
+                end = start + length + (rangeEnd-rangeStart);
+                ranges[j] = Range(-1,-1); // Delete
             }
         }
-        return finalId;
+        for(uint pos = lastIndex; pos > positionToAssume; pos--){
+            ranges[pos] = ranges[pos-1];
+        }
+        ranges[positionToAssume] = Range(start, min(end, intTOKEN_LIMIT));
+        lastIndex++;
+        if(end > intTOKEN_LIMIT){
+            addRange(ranges, 0, end - intTOKEN_LIMIT, lastIndex);
+            lastIndex++;
+        }
+        return lastIndex;
+    }
+
+    function buildJumps(uint lastBatch) view private returns (Range[RANGE_LENGTH] memory) {
+        Range[RANGE_LENGTH] memory ranges;
+        uint lastIndex = 0;
+        for(uint i=0; i<lastBatch; i++){
+            int128 start = int128(int(batchToSeed[i]));
+            int128 end = int128(int(batchToSeed[i] + REVEAL_BATCH_SIZE));
+            lastIndex = addRange(ranges, start, end, lastIndex);
+        }
+        return ranges;
+    }
+
+    function getShuffledTokenId(uint startId, uint batch) view private returns (uint) {
+        Range[RANGE_LENGTH] memory ranges = buildJumps(batch);
+        int128 positionsToMove = int128(int(startId % REVEAL_BATCH_SIZE));
+        int128 id = int128(int(batchToSeed[batch]));
+
+        for(uint round = 0; round<2; round++){
+            for(uint i=0; i<RANGE_LENGTH; i++){
+                int128 start = ranges[i].start;
+                int128 end = ranges[i].end;
+                if(id < start){
+                    int128 finalId = id + positionsToMove;
+                    if(finalId < start){
+                        return uint(uint128(finalId));
+                    } else {
+                        positionsToMove -= start - id;
+                        id = end;
+                    }
+                } else if(id < end){
+                    id = end;
+                }
+            }
+            if((id + positionsToMove) >= intTOKEN_LIMIT){
+                positionsToMove -= (intTOKEN_LIMIT - id) + 1;
+                id = 0;
+            }
+            id = id % intTOKEN_LIMIT;
+        }
+        return uint(uint128(id + positionsToMove));
     }
 }
